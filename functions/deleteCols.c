49c9388a3b99f63ec54ee279730f1194b2f54881
Guilherme Souza <gui.brito.souza@gmail.com>
2024-03-07 15:25:50 +0000

    Deixando mais modularizado

--- functions/deleteCols.c
+++ functions/deleteCols.c
@@ -1,47 +1,5 @@
 #include "../matrix.h"
 
-
-//Função que deleta a linha desejada da matriz predefinida
-Matrix *deleteRows(Matrix *matrix, int rowstoDelete){
-    int rowsDelete = matrix->rows - rowstoDelete;
-    int cols = matrix->cols;
-
-    //Aloca memória para a nova matriz expandida
-    int **newData = (int **)malloc(rowsDelete * sizeof(int *));
-    if(newData == NULL){
-        printf("Erro ao alocar memória para a nova matriz.\n");
-        return NULL;
-    }
-
-    for (int i = 0; i < matrix->rows; i++) {
-        newData[i] = (int *)malloc(cols * sizeof(int));
-        if (newData[i] == NULL) {
-            printf("Erro ao alocar memória para a nova matriz.\n");
-            // Libera a memória alocada anteriormente
-            for (int j = 0; j < i; j++) {
-                free(newData[j]);
-            }
-            free(newData);
-            return NULL;
-        }
-        for (int j = 0; j < cols; j++) {
-            newData[i][j] = matrix->data[i][j];
-        }
-    }
-    
-     // Libera a memória alocada para a matriz original
-    for (int i = 0; i < matrix->rows; i++) {
-        free(matrix->data[i]);
-    }
-    free(matrix->data);
-
-    // Atribui a nova matriz expandida à estrutura Matrix
-    matrix->rows = rowsDelete;
-    matrix->data = newData;
-
-    return matrix;
-}
-
 Matrix *deleteCols(Matrix *matrix, int colstoDelete) {
     int colsDelete = matrix->cols - colstoDelete;
     int rows = matrix->rows;
