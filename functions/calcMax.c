89556f22ccdb4cd13ae3f210f5bd9c027f5381d1
Guilherme Souza <gui.brito.souza@gmail.com>
2024-03-07 16:05:08 +0000

    Trabalhando na calcMax

--- functions/calcMax.c
+++ functions/calcMax.c
@@ -10,13 +10,13 @@ int calcMax(Matrix *matrix, int rows, int cols, int *totalSum) {
             // Calcula a soma máxima considerando apenas os valores que não compartilham linha ou coluna
             int sum = matrix->data[i][j];
             if (i > 0) {
-                sum -= matrix->data[i - 1][j];
+                sum += matrix->data[i - 1][j];
             }
             if (j > 0) {
-                sum -= matrix->data[i][j - 1];
+                sum += matrix->data[i][j - 1];
             }
             if (i > 0 && j > 0) {
-                sum += matrix->data[i - 1][j - 1];
+                sum -= matrix->data[i - 1][j - 1];
             }
 
             // Atualiza a soma máxima encontrada até o momento
