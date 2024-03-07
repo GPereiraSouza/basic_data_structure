2a177ddded2fac834d588505ed620d5a55e0a0f3
Guilherme Souza <gui.brito.souza@gmail.com>
2024-03-07 15:11:03 +0000

    Deixando mais modularizado

--- functions/showMatrix.c
+++ functions/showMatrix.c
@@ -1,17 +1,5 @@
 #include "../matrix.h"
 
-void loadPredefinedMatrix(Matrix *matrix) {
-    // Verifica se a estrutura da matriz e a matriz com os dados dados são válidas
-    if (matrix != NULL && matrix->data != NULL) {
-        // Copia os valores da matriz pré-definida para a matriz de dados
-        for (int i = 0; i < matrix->rows; i++) {
-            for (int j = 0; j < matrix->cols; j++) {
-                matrix->data[i][j] = predefinedMatrix[i][j];
-            }
-        }
-    }
-}
-
 void showMatrix(Matrix *matrix) {
     // Itera sobre cada linha da matriz
     for (int i = 0; i < matrix->rows; i++) {
